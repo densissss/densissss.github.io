5028cb1af751c008ff8a01e048fa3ba42d72b1f7
densissss <dennis.versyck@gmail.com>
2023-12-12 21:48:46 +0100

    alles functioneel

--- manillen/script.js
+++ manillen/script.js
@@ -10,9 +10,13 @@ function opschrijvenLinks() {
         scoreLijstL.push(parseInt(scoreTotaalL.innerHTML) + scoreNieuwL); 
         scoreOudL.innerHTML = scoreLijstL.slice(0, scoreLijstL.length - 1).join('<br>');
         scoreOudL.classList.add("strike");
+        console.log(scoreLijstL)
         scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) + scoreNieuwL;
-        document.getElementById("puntenl").value = "";
-    } 
+        // somL = scoreLijstL.reduce((a, b) => a + b, 0);
+        
+        document.getElementById("puntenl").value = ""; //input resetten naar placeholder
+        return scoreLijstL        
+} 
     
     
 }
@@ -27,9 +31,39 @@ function opschrijvenRechts() {
         scoreOudR.classList.add("strike");
         scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) + scoreNieuwR;
         document.getElementById("puntenr").value = "";
+        console.log(scoreLijstR)
+        return scoreLijstR
+        
     }    
 }
    
+function undoLinks() {
+    var zeker = confirm("Zie je zeker daje gemist ziet?");
+    if (zeker == true) {
+        var scoreTotaalL = document.getElementById("totaalLinks");
+        scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) - (scoreLijstL[scoreLijstL.length-1] - scoreLijstL[scoreLijstL.length-2]);
+        scoreLijstL.pop()
+        document.getElementById("scoresOudLinks").innerHTML = scoreLijstL.slice(0, scoreLijstL.length - 1).join('<br>');
+        console.log(scoreLijstL)
+        if (isNaN(scoreTotaalL.innerHTML)) {
+            scoreTotaalL.innerHTML="0"
+        }  
+    }  
+    
+}
+
+function undoRechts() {
+    var zeker = confirm("Zie je zeker daje gemist ziet?");
+    if (zeker == true) {
+        var scoreTotaalR = document.getElementById("totaalRechts");
+        scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) - (scoreLijstR[scoreLijstR.length-1] - scoreLijstR[scoreLijstR.length-2]);
+        scoreLijstR.pop()
+        document.getElementById("scoresOudRechts").innerHTML = scoreLijstR.slice(0, scoreLijstR.length - 1).join('<br>');
+        if (isNaN(scoreTotaalR.innerHTML)) {
+        scoreTotaalR.innerHTML="0"
+        }
+    }
+}
 
 function reset() {
     var zeker = confirm("Zie je zeker daje alle puntn wilt wegdoen?");
@@ -37,4 +71,3 @@ function reset() {
         location.reload();
     }     
 }
-
