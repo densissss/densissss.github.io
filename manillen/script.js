2a2ef47d8b4e564d156ad30c6f052313500b7027
densissss <dennis.versyck@gmail.com>
2023-12-12 18:52:53 +0100

    manillen

---
+++ manillen/script.js
@@ -0,0 +1,34 @@
+
+const scoreLijstL = [0];
+const scoreLijstR = [0];
+
+function opschrijvenLinks() {
+    var scoreNieuwL = parseInt(document.getElementById("puntenl").value);
+    var scoreOudL = document.getElementById("scoresOudLinks");
+    var scoreTotaalL = document.getElementById("totaalLinks");
+    scoreLijstL.push(parseInt(scoreTotaalL.innerHTML) + scoreNieuwL); 
+    scoreOudL.innerHTML = scoreLijstL.slice(0, scoreLijstL.length - 1).join('<br>');
+    scoreOudL.classList.add("strike");
+    scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) + scoreNieuwL;
+}
+
+function opschrijvenRechts() {
+    var scoreNieuwR = parseInt(document.getElementById("puntenr").value);
+    var scoreOudR = document.getElementById("scoresOudRechts");
+    var scoreTotaalR = document.getElementById("totaalRechts");
+    scoreLijstR.push(parseInt(scoreTotaalR.innerHTML) + scoreNieuwR); 
+    scoreOudR.innerHTML = scoreLijstR.slice(0, scoreLijstR.length - 1).join('<br>');
+    scoreOudR.classList.add("strike");
+    scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) + scoreNieuwR;
+}
+
+function reset() {
+    document.getElementById("scoresOudRechts").innerHTML = "0";   
+    document.getElementById("scoresOudLinks").innerHTML = "0";
+    document.getElementById("totaalLinks").innerHTML = "";
+    document.getElementById("totaalRechts").innerHTML = "";
+    document.getElementById("scoresOudLinks").classList.remove("strike");
+    document.getElementById("scoresOudRechts").classList.remove("strike");
+
+
+}
