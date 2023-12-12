3cc8b296f1f95552bd30fc6a51a84f34686e917b
densissss <dennis.versyck@gmail.com>
2023-12-12 20:05:06 +0100

    verbeteringen

--- manillen/script.js
+++ manillen/script.js
@@ -6,21 +6,30 @@ function opschrijvenLinks() {
     var scoreNieuwL = parseInt(document.getElementById("puntenl").value);
     var scoreOudL = document.getElementById("scoresOudLinks");
     var scoreTotaalL = document.getElementById("totaalLinks");
-    scoreLijstL.push(parseInt(scoreTotaalL.innerHTML) + scoreNieuwL); 
-    scoreOudL.innerHTML = scoreLijstL.slice(0, scoreLijstL.length - 1).join('<br>');
-    scoreOudL.classList.add("strike");
-    scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) + scoreNieuwL;
+    if (isNaN(scoreNieuwL) === false ) { 
+        scoreLijstL.push(parseInt(scoreTotaalL.innerHTML) + scoreNieuwL); 
+        scoreOudL.innerHTML = scoreLijstL.slice(0, scoreLijstL.length - 1).join('<br>');
+        scoreOudL.classList.add("strike");
+        scoreTotaalL.innerHTML = parseInt(scoreTotaalL.innerHTML) + scoreNieuwL;
+        document.getElementById("puntenl").value = "";
+    } 
+    
+    
 }
 
 function opschrijvenRechts() {
     var scoreNieuwR = parseInt(document.getElementById("puntenr").value);
     var scoreOudR = document.getElementById("scoresOudRechts");
     var scoreTotaalR = document.getElementById("totaalRechts");
-    scoreLijstR.push(parseInt(scoreTotaalR.innerHTML) + scoreNieuwR); 
-    scoreOudR.innerHTML = scoreLijstR.slice(0, scoreLijstR.length - 1).join('<br>');
-    scoreOudR.classList.add("strike");
-    scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) + scoreNieuwR;
+    if (isNaN(scoreNieuwR) === false) {
+        scoreLijstR.push(parseInt(scoreTotaalR.innerHTML) + scoreNieuwR); 
+        scoreOudR.innerHTML = scoreLijstR.slice(0, scoreLijstR.length - 1).join('<br>');
+        scoreOudR.classList.add("strike");
+        scoreTotaalR.innerHTML = parseInt(scoreTotaalR.innerHTML) + scoreNieuwR;
+        document.getElementById("puntenr").value = "";
+    }    
 }
+   
 
 function reset() {
     var zeker = confirm("Zie je zeker daje alle puntn wilt wegdoen?");
@@ -28,3 +37,4 @@ function reset() {
         location.reload();
     }     
 }
+
